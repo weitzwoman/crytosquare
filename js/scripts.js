8166df054c02e694295cca3b5a643f9f6ab0ac4c
Brian Pritt and Tracie Weitzman <brian@>
2016-10-27 16:55:23 -0700

    add column length and width functionality, and transform string

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,25 @@ function hasPunctuation(punctuationString) {
   var unpunctuated = punctuationString.replace(/\W/g,"");
   return unpunctuated.toLowerCase();
 }
-
-
+function gridLayout(stringToGrid){
+  var rowLength;
+  var columnLength = Math.floor(Math.sqrt(stringToGrid.length));
+  console.log(columnLength);
+  if ((stringToGrid % columnLength) != 0){
+    rowLength = columnLength +1;
+    console.log(rowLength);
+  }else{
+    rowLength = columnLength;
+    console.log(rowLength);
+  }
+}
 $(document).ready(function() {
   $("form#encrypt").submit(function(event){
      event.preventDefault();
 
     var cryptoInput = $("#crypto").val();
-    var finalEncrypted = hasPunctuation(cryptoInput);
+    var removePunctuation = hasPunctuation(cryptoInput);
+    var finalEncrypted = gridLayout(removePunctuation);
     //$("#output").show();
     $("#result").text(finalEncrypted);
     });
